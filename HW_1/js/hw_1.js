5f11c5c6de9539810171f88279930465b3479428
Anny Anny <58954581+anyt04ka@users.noreply.github.com>
2021-10-21 13:43:42 +0300

    Add files via upload
    home work 1

---
+++ HW_1/js/hw_1.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// задание 1, формула S = 2 (a · b + a · с + b · с) 
+
+let height = 2;
+let length = 5;
+let width = 3;
+let square = 2 * (height * length + height * width + length * width); 
+
+console.log(square);
+
+// задание 2 
+
+let n = 56;
+let firstN = n / 10;
+let secondN = n % 10;
+let sum = firstN + secondN;
+
+console.log(sum);
+
+// задание 3 
+
+let a = 7;
+let b = 3; 
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log(a, b); 
